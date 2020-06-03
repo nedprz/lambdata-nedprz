128721aaceecc865a802255d01c33b816c075103
nedprz <16900857+nedprz@users.noreply.github.com>
2020-06-03 07:48:50 -0700

    yo

--- my_lambdata/geo.py
+++ my_lambdata/geo.py
@@ -2,16 +2,12 @@
 
 def pythag(a , b):
     """
-    Param n is a number
-    Function will enlarge the number
+    Params a and b are numbers
+    Function will find length of side c of pythagorean triangle
     """
     return (a**2 + b**2)**.5
 
-# this code breaks our ability to import enlarge from other files, if left in the global scope:
-#
-# print("HELLO")
-# y = int(input("Please choose a number"))
-# print(y, enlarge(y))
+
 
 if __name__ == "__main__":
     # only run the code below IF this script is invoked from the command-line
@@ -20,3 +16,27 @@ if __name__ == "__main__":
     a = int(input("A: "))
     b= int(input("B: "))
     print(pythag(a,b))
+
+
+
+
+def dist(lat1,long1,lat2,long2):
+    """
+    Params are longitude and latitude values (floats)
+
+    Function will generate the distance between them (in degrees)
+    """
+    return ((lat2-lat1)**2 + (long2-long1)**2)**.5
+
+
+
+if __name__ == "__main__":
+    # only run the code below IF this script is invoked from the command-line
+    # not if it is imported from another script
+    print("HELLO")
+    lat1 = float(input("lat1: "))
+    long1= float(input("long1: "))
+    lat2 = float(input("lat2: "))
+    long2 = float(input("long2: "))
+
+    print(dist(lat1,long1,lat2,long2))
